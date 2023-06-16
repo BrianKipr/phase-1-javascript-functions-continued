f56c1402e8245d78c4e30d5fdf07477b03843be9
Briankipr <brian.kiprono1@student.moringaschool.com>
2023-06-16 21:50:38 +0300

    phase-1-javascript-functions-continued

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+   if (Activity = "bathe my dog") {
+    return "This Saturday, I want to bathe my dog!!";
+   }else{
+    return "This Saturday, I want to roller-skate!"
+   }
+}
+function mondayWork(work) {
+  if ((activity = "go to the office")) {
+    return "This Saturday, I want to bathe my dog!.";
+  }else{
+    return "This Monday, I will work from home.!"
+  }
+}
+function wrapAdjective(flair = "*") {
+  return function (adjective) {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
